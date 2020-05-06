b713bd24915ce4c781c1f3b8bb12b325b1678072
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-06 20:26:42 +0900

    switch문으로 작성

--- likeswitch/Program.cs
+++ likeswitch/Program.cs
@@ -10,6 +10,23 @@ namespace likeswitch
     {
         static void Main(string[] args)
         {
+            Console.Write("학년을 입력하세요: ");
+            int level = int.Parse(Console.ReadLine());
+
+            Console.Write("수강해야 하는 전공 학점: ");
+            switch(level)
+            {
+                case 1:
+                    Console.WriteLine("12학점");
+                    break;
+                case 2:
+                case 4:
+                    Console.WriteLine("18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("10학점");
+                    break;
+            }
         }
     }
 }
